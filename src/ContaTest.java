5ef78e2e3cde8aa0d389777272587894c1965f14
caiocmsouza <caiocesar.msouza@gmail.com>
2020-01-27 14:18:12 -0200

    aula01.video03

---
+++ src/ContaTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.Test;
+
+class ContaTest {
+
+	@Test
+	public void aoSacarUmValorDeUmaContaOSaldoDeveSofrerUmaSubtracaoDesseValor() {
+		Conta conta = new Conta(200d);
+		conta.sacar(50d);
+		
+		
+		assertThat(conta.getSaldo(), is(equalTo(150d)));
+		
+	}
+
+}
